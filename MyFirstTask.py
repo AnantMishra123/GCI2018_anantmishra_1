08f50a693286b59ff3d4a0c3a880c09448c5218e
AnantMishra123 <44678287+AnantMishra123@users.noreply.github.com>
2018-11-03 00:43:20 +0530

    Add files via upload

---
+++ MyFirstTask.py
@@ -0,0 +1,30 @@
+<<<<<<< HEAD
+for i in range(0,10):
+    print("GCI is great")
+# Python code to reverse a string
+# using loop
+
+def reverse(s):
+    str = ""
+    for i in s:
+        str = i + str
+    return str
+
+name=input("what is your name? ")
+print ("Hello",name,"pleased to meet you!")
+=======
+for i in range(0,10):
+    print("GCI is great")
+# Python code to reverse a string
+# using loop
+
+def reverse(s):
+    str = ""
+    for i in s:
+        str = i + str
+    return str
+
+name=input("what is your name? ")
+print ("Hello",name,"pleased to meet you!")
+>>>>>>> e88ae7dccefaad4d99d1e0011407053bf4e78128
+print ("Did you know your name backwards is",reverse(name),"?")
